ddfd3179e3965af9dcace5145a2a9d094e71a6ed
박수정 <sujung26@tmon.co.kr>
2019-10-31 16:36:36 +0900

    - launchMode 실험

--- app/src/main/java/com/sujungp/lifecycletestapp/ActivityC.kt
+++ app/src/main/java/com/sujungp/lifecycletestapp/ActivityC.kt
@@ -5,75 +5,72 @@ import android.content.res.Configuration
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_b.*
+import kotlinx.android.synthetic.main.activity_c.*
 
 /**
- * Created by sujung26 on 2019-10-24.
+ * Created by sujung26 on 2019-10-31.
  */
-class ActivityB : AppCompatActivity() {
+class ActivityC : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_b)
-        Log.d("sujung", "[Activity B] onCreate")
+        setContentView(R.layout.activity_c)
+        Log.d("sujung", "[Activity C] onCreate")
 
-        intent?.let {
-            val num = it.getIntExtra("a", 1)
-            numText.text = num.toString()
-        }
-
-        goCBtn.setOnClickListener {
-            val intent = Intent(this, ActivityC::class.java)
+        goBBtn.setOnClickListener {
+            val intent = Intent(this, ActivityB::class.java).apply {
+                putExtra("b", 2)
+            }
             startActivity(intent)
         }
     }
 
     override fun onRestart() {
         super.onRestart()
-        Log.d("sujung", "[Activity B] onRestart")
+        Log.d("sujung", "[Activity C] onRestart")
     }
 
     override fun onStart() {
         super.onStart()
-        Log.d("sujung", "[Activity B] onStart")
+        Log.d("sujung", "[Activity C] onStart")
     }
 
     override fun onResume() {
         super.onResume()
-        Log.d("sujung", "[Activity B] onResume")
+        Log.d("sujung", "[Activity C] onResume")
     }
 
     override fun onPause() {
         super.onPause()
-        Log.d("sujung", "[Activity B] onPause")
+        Log.d("sujung", "[Activity C] onPause")
     }
 
     override fun onStop() {
         super.onStop()
-        Log.d("sujung", "[Activity B] onStop")
+        Log.d("sujung", "[Activity C] onStop")
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        Log.d("sujung", "[Activity B] onDestroy")
+        Log.d("sujung", "[Activity C] onDestroy")
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
-        Log.d("sujung", "[Activity B] onSaveInstanceState")
+        Log.d("sujung", "[Activity C] onSaveInstanceState")
         super.onSaveInstanceState(outState)
     }
 
     override fun onNewIntent(intent: Intent?) {
-        Log.d("sujung", "[Activity B] onNewIntent")
+        Log.d("sujung", "[Activity C] onNewIntent")
         super.onNewIntent(intent)
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
-        Log.d("sujung", "[Activity B] onRestoreInstanceState")
+        Log.d("sujung", "[Activity C] onRestoreInstanceState")
         super.onRestoreInstanceState(savedInstanceState)
     }
 
     override fun onConfigurationChanged(newConfig: Configuration) {
-        Log.d("sujung", "[Activity B] onConfigurationChanged")
+        Log.d("sujung", "[Activity C] onConfigurationChanged")
         super.onConfigurationChanged(newConfig)
     }
 }
